d91216ff3016f9a00619f48f57e6cbfe6e58f0f6
Sushant Saurav <sushant.saurav@wissen.com>
2022-03-06 17:07:18 +0530

    add test cases

--- src/main/TableTennisScoring.java
+++ src/main/TableTennisScoring.java
@@ -5,77 +5,115 @@ public class TableTennisScoring {
 
 	public static void main(String[] args) {
 		
-		int scoreP1=0 , scoreP2=0;
+		
 		boolean isPlayer1 = false;
-		int countOfServe = 0;
-		
+	
 		//use toss to start match
-		int toss = toss();
-		isPlayer1 = (toss == 0 ? true :false);
-		boolean isRule10Active = false;
-		String winner = null;
+		//true if player
+		isPlayer1 = toss(generateRandomNum());
 		
-		// Break only when winner is decided
-		while (true) {
-			if(isPlayer1) {
-				countOfServe++;
-				scoreP1+=generateRandomNum()%2==0?1:0;
-				
-			}
-			else {
-				countOfServe++;
-				scoreP2+=generateRandomNum()%2==1?1:0;
-				
-			}
-			
-			System.out.println("Current Score of P1 : "+scoreP1);
-			System.out.println("Current Score of P2 : "+scoreP2);
-			
-			if(countOfServe == 2) {
-				isPlayer1 = isPlayer1 == true ? false : true;
-				countOfServe = 0;
-			}
-			
-			if(scoreP1==10 && scoreP2==10)
-				isRule10Active = true;
-			else if(scoreP1 == 20 && scoreP2 == 20)
-				isRule10Active = false;
-			if(scoreP1 > 10 || scoreP2 >10) {
-				winner = checkWhoWins(scoreP1,scoreP2,isRule10Active) ;
-			}
-			
-			if(winner != null)
-				break;
-				
-		}
+		String winner = startPlay(isPlayer1);
 		
 		System.out.println("Congratulations !! Player :"+winner+" you have done it.");
 
 	}
 	
-	//logic to decide who wins the match as per scoring rules
-	private static String checkWhoWins(int scoreP1, int scoreP2,boolean isRule10Active) {
-			
-		if(isRule10Active) {
-				if(Math.abs(scoreP2-scoreP1) >=2 )
-					return (scoreP2>scoreP1 ? "P2" : "P1");
-			}
-		else if(Math.abs(scoreP2-scoreP1) >=1){
-			return (scoreP2>scoreP1 ? "P2" : "P1");
-		}
-		return null;
+	/**
+	 * Driver function to start match and keep playing
+	 * 
+	 * @param isPlayer1 
+	 * @return winner name
+	 */
+	public static String startPlay(boolean isPlayer1) {
+		
+		int scoreP1=0 , scoreP2=0;
+		boolean isRule10Active = false;
+		int countOfServe = 0;
+		String winner = null;
+		
+		// Break only when winner is decided
+				while (true) {
+					if(isPlayer1) {
+						countOfServe++;
+					}
+					else {
+						countOfServe++;
+					}
+					
+					if(generateRandomNum()%2==0)
+						scoreP1++;
+					else
+						scoreP2++;
+					
+					System.out.println("Current Score of P1 : "+scoreP1);
+					System.out.println("Current Score of P2 : "+scoreP2);
+					
+					if(countOfServe == 2) {
+						isPlayer1 = isPlayer1 == true ? false : true;
+						countOfServe = 0;
+					}
+					
+					isRule10Active = isRule10Active(scoreP1, scoreP2, isRule10Active);
+					
+					if(scoreP1 > 10 || scoreP2 >10) {
+						winner = checkWhoWins(scoreP1,scoreP2,isRule10Active) ;
+					}
+					
+					if(winner != null)
+						break;	
+				}
+				return winner;
+				
 	}
 
-	//util function to generate Random integers
-	public static int generateRandomNum() {
+	/**
+	 * Function useful when tie is there at score 10 or 20
+	 * 
+	 * @param scoreP1
+	 * @param scoreP2
+	 * @param isRule10Active
+	 * @return true if difference of 2 is required for winning
+	 */
+	public static boolean isRule10Active(int scoreP1, int scoreP2, boolean isRule10Active) {
+		if(scoreP1==10 && scoreP2==10)
+			isRule10Active = true;
+		else if(scoreP1 == 20 && scoreP2 == 20)
+			isRule10Active = false;
+		return isRule10Active;
+	}
+
+	/**
+	 * logic to decide who wins the match as per scoring rules
+	 * 
+	 * @param scoreP1 
+	 * @param scoreP2
+	 * @param isRule10Active
+	 * @return
+	 */
+	public static String checkWhoWins(int scoreP1, int scoreP2,boolean isRule10Active) {
+		String winner = null;
+			
+		if(isRule10Active) {
+				if(Math.abs(scoreP2-scoreP1) >=2 ) {
+					winner = (scoreP2>scoreP1 ? "P2" : "P1");
+				}	
+			}
+		else if(Math.abs(scoreP2-scoreP1) >=1){
+			winner = (scoreP2>scoreP1 ? "P2" : "P1");
+			return winner;
+		}
+		return winner;
+	}
+
+	//Util function to generate Random integers
+	private static int generateRandomNum() {
 		Random rand = new Random();
 		return rand.nextInt();
 	}
 	
 	// Method for toss so as to decide which player starts serving first
-	public static int toss() {
-		Random rand = new Random();
-		return rand.nextInt(2);
+	public static boolean toss(int randNum) {
+		return (randNum % 2 == 0 ? true :false);
 	}
 
 	
--- src/test/TableTennisScoringTest.java
+++ src/test/TableTennisScoringTest.java
@@ -1,14 +1,35 @@
 package test;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-public class TableTennisScoringTest {
+import junit.framework.Assert;
+import main.TableTennisScoring;
+
+class TableTennisScoringTest {
 
 	@Test
-	public void test() {
+	void testStartPlay() {
 		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCheckWhoWins() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testTossForEven() {
+		
+		boolean res = TableTennisScoring.toss(4444);
+		assertEquals(res, true);
+	}
+	
+	@Test
+	void testTossForOdd() {
+		boolean res = TableTennisScoring.toss(5555);
+		assertEquals(res, false);
 	}
 
 }
