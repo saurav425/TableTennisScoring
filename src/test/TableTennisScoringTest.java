37d27263d6ee4543b2f6f86465caf330e944869c
Sushant Saurav <sushant.saurav@wissen.com>
2022-03-06 13:22:03 +0530

    add test class

--- src/test/TableTennisScoringTest.java
+++ src/test/TableTennisScoringTest.java
@@ -1,13 +1,13 @@
 package test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-class TableTennisScoringTest {
+public class TableTennisScoringTest {
 
 	@Test
-	void test() {
+	public void test() {
 		fail("Not yet implemented");
 	}
 
